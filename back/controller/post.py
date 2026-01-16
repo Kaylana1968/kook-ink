58215fe1649460462b40dbdbf88a77db84b5a1ca
Ilona Roussel <fleur838@gmail.com>
2026-01-16 23:22:13 +0100

    Les post de l'utilisateur s'affiche dans la home mais pas les recette

--- back/controller/post.py
+++ back/controller/post.py
@@ -9,6 +9,22 @@ from pydantic import BaseModel
 class PostCreate(BaseModel):
     description: str
     
+    
+# GET ALL POST
+@router.get("/post")
+def get_posts(db: Session = Depends(database.get_db)):
+    posts = db.query(models.Post).limit(10).all()
+
+    to_return = []
+    for post in posts: 
+        user = db.get(models.User, post.user_id)
+        to_return.append({
+            "description": post.description,
+            "user": user
+        })
+
+    return {"posts": to_return}
+
 # CREATE A POST
 @router.post("/post")
 def upload_recipe(post: PostCreate, user=Depends(utils.get_user), db: Session = Depends(database.get_db)):
--- front/lib/home_screen.dart
+++ front/lib/home_screen.dart
@@ -10,25 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Future<List<dynamic>> _recipeFuture;
+  late Future<List<dynamic>> _postFuture;
 
   @override
   void initState() {
     super.initState();
-    _recipeFuture = fetchRecipes();
+    _postFuture = fetchPosts();
   }
 
-  Future<List<dynamic>> fetchRecipes() async {
+  Future<List<dynamic>> fetchPosts() async {
     try {
       final response = await http.get(
-        Uri.parse('http://127.0.0.1:8000/recipe'),
+        Uri.parse('http://127.0.0.1:8000/post'),
         headers: {"Content-Type": "application/json"},
       );
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = jsonDecode(response.body);
 
-        return data['recipes'] as List<dynamic>;
+        return data['posts'] as List<dynamic>;
       } else {
         throw Exception('Erreur serveur: ${response.statusCode}');
       }
@@ -39,14 +39,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<void> _refresh() async {
     setState(() {
-      _recipeFuture = fetchRecipes();
+      _postFuture = fetchPosts();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<dynamic>>(
-      future: _recipeFuture,
+      future: _postFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
@@ -61,14 +61,14 @@ class _HomeScreenState extends State<HomeScreen> {
           return _buildEmptyState();
         }
 
-        final recipes = snapshot.data!;
+        final posts = snapshot.data!;
 
         return RefreshIndicator(
           onRefresh: _refresh,
           color: Colors.orange,
           child: ListView.builder(
-            itemCount: recipes.length,
-            itemBuilder: (context, index) => RecipePost(recipe: recipes[index]),
+            itemCount: posts.length,
+            itemBuilder: (context, index) => RecipePost(post: posts[index]),
           ),
         );
       },
@@ -98,8 +98,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
 // POST COMPONENT
 class RecipePost extends StatelessWidget {
-  final Map<String, dynamic> recipe;
-  const RecipePost({super.key, required this.recipe});
+  final Map<String, dynamic> post;
+  const RecipePost({super.key, required this.post});
 
   @override
   Widget build(BuildContext context) {
@@ -107,52 +107,19 @@ class RecipePost extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         // Header (User Info)
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
           child: Row(
             children: [
-              const CircleAvatar(
+              CircleAvatar(
                 radius: 16,
                 backgroundColor: Colors.orange,
                 child:
                     Icon(Icons.restaurant_menu, size: 18, color: Colors.white),
               ),
-              const SizedBox(width: 10),
-              Text(
-                recipe['user']['username'],
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-              const Icon(Icons.more_horiz),
-            ],
-          ),
-        ),
-
-        // Image
-        AspectRatio(
-          aspectRatio: 1,
-          child: Image.network(
-            recipe['image_link'],
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) => Container(
-              color: Colors.grey[200],
-              child:
-                  const Icon(Icons.broken_image, size: 50, color: Colors.grey),
-            ),
-          ),
-        ),
-
-        // Buttons(icons)
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-          child: Row(
-            children: [
-              Icon(Icons.favorite_border, size: 28),
-              SizedBox(width: 15),
-              Icon(Icons.chat_bubble_outline, size: 26),
-              SizedBox(width: 15),
+              SizedBox(width: 10),
               Spacer(),
-              Icon(Icons.bookmark_border, size: 28),
+              Icon(Icons.more_horiz),
             ],
           ),
         ),
@@ -165,51 +132,16 @@ class RecipePost extends StatelessWidget {
             children: [
               // Recipe Name
               Text(
-                recipe['name'],
+                post['description'],
                 style:
                     const TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
               ),
               const SizedBox(height: 4),
-
-              // Time, Difficulty, Servings
-              Row(
-                children: [
-                  _infoChip(
-                    Icons.timer_outlined,
-                    "${recipe['preparation_time'] ?? 0} min",
-                  ),
-                  const SizedBox(width: 12),
-                  _infoChip(
-                    Icons.local_fire_department_outlined,
-                    "${recipe['baking_time'] ?? 0} min",
-                  ),
-                  const SizedBox(width: 12),
-                  _infoChip(Icons.trending_up, "Niv. ${recipe['difficulty']}"),
-                  const SizedBox(width: 12),
-                  _infoChip(Icons.people_outline, "${recipe['person']} pers."),
-                ],
-              ),
             ],
           ),
         ),
-        const SizedBox(height: 24), // Space posts
+        const SizedBox(height: 24),
         const Divider(height: 1, thickness: 0.5),
-      ],
-    );
-  }
-
-  Widget _infoChip(IconData icon, String label) {
-    return Row(
-      children: [
-        Icon(icon, size: 14, color: Colors.grey[600]),
-        const SizedBox(width: 4),
-        Text(
-          label,
-          style: TextStyle(
-              color: Colors.grey[600],
-              fontSize: 12,
-              fontWeight: FontWeight.w500),
-        ),
       ],
     );
   }
