3774e72ff32e04658af42537f632ebae01d65016
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2026-04-28 15:36:50 +0200

    changed tests so it doesn't keep the data of the database in the files but wipe it after test

--- back/test/test_main.py
+++ back/test/test_main.py
@@ -1,20 +1,24 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 from main import app
 from common.database import Base
 
 client = TestClient(app)
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool, # StaticPool so the db is not wiped after first test
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create the tables using the same Base as the real database
 Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = TestingSessionLocal()
